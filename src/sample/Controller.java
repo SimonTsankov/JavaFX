cca99aea747dc9d2a432c0227637f0820074fe38
SimonTsankov <simoncankov@gmail.com>
2020-11-18 14:04:27 +0200

    initial

---
+++ src/sample/Controller.java
@@ -0,0 +1,103 @@
+package sample;
+
+import java.awt.event.ActionListener;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    boolean nameBool=false;
+    boolean priceBool = false;
+    String data[]=new String[100];
+    int numAded=0;
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField price;
+
+    @FXML
+    private Button btnAdd;
+    @FXML
+    private Button btnQuit;
+
+    @FXML
+    private Button btnRemove;
+    @FXML
+    private Label label;
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+         label.setText("Products list:\n");
+
+    }
+
+    @FXML
+    void btnADD(ActionEvent event) {
+            label.setText(label.getText()+name.getText()+" - "+price.getText()+"\n");
+            data[numAded]=label.getText();
+            numAded++;
+    }
+    @FXML
+    void nameClear(){
+        name.setText("");
+    }
+    @FXML
+    void priceClear(){
+        price.setText("");
+    }
+
+    @FXML//PRESING ENTER WHILE TYPING NAME
+    void nameEntered(){
+        if(nameBool!=true) {
+            label.setText(label.getText() + name.getText()+" - ");
+            nameBool = true;
+            priceBool=false;
+            data[numAded]=label.getText();
+            numAded++;
+        }
+    }
+    @FXML//PRESING ENTER WHILE TYPING PRICE
+    void priceEntered(){
+        if(priceBool!=true) {
+            label.setText(label.getText() + price.getText()+"\n");
+            priceBool=true;
+            nameBool=false;
+            data[numAded]=label.getText();
+        }
+        }
+
+    @FXML//EXIT
+    void btnQuitOnAction(ActionEvent event) {
+        Platform.exit();
+
+    }
+    @FXML//REMOVES LAST ADDED
+    void btnRemoveOnAction(ActionEvent event){
+        //TO DO
+
+    }
+
+    @FXML
+    void initialize() {
+        assert  label !=null : "fx:id=\"label\" was not injected: check your FXML file 'sample.fxml'.";
+        assert btnClear != null : "fx:id=\"btnClear\" was not injected: check your FXML file 'sample.fxml'.";
+        assert btnQuit != null : "fx:id=\"btnQuit\" was not injected: check your FXML file 'sample.fxml'.";
+        label.setText("Products list:\n");
+        assert btnRemove != null : "fx:id=\"btnRemove\" was not injected: check your FXML file 'sample.fxml'.";
+        assert btnAdd != null : "fx:id=\"btnAdd\" was not injected: check your FXML file 'sample.fxml'.";
+        assert name != null : "fx:id=\"name\" was not injected: check your FXML file 'sample.fxml'.";
+        assert price != null : "fx:id=\"price\" was not injected: check your FXML file 'sample.fxml'.";
+
+    }
+}
